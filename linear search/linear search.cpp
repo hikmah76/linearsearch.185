cb951a1076f63fe99e1c21ecbff4aa9430697af7
nur hikmah <nur.hikmah190504@gmail.com>
2023-04-14 10:45:26 +0700

    // Accep array elements     cout << "\n----------------\n";     cout << "Enter Array elements \n";

--- linear search/linear search.cpp
+++ linear search/linear search.cpp
@@ -18,11 +18,18 @@ void input()
         else
             cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
     }
+
+    // Accep array elements 
+    cout << "\n----------------\n";
+    cout << "Enter Array elements \n";
+    cout << "-------------------\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << "> ";
+        cin >> arr[i];
+    }
 }
 
-int main()
-{
-    std::cout << "Hello World!\n should";
-}
+
 
 
