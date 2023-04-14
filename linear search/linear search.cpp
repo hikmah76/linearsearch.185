bdb8abb4eeaef2830779acc01fafa47036cb1cf6
nur hikmah <nur.hikmah190504@gmail.com>
2023-04-14 10:30:29 +0700

    #include <iostream> using namespace std;

--- linear search/linear search.cpp
+++ linear search/linear search.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+
+int arr[20]; //Array to be searched
+int n; // Number of elements in the array
+int i; //Index of array element
+
 int main()
 {
     std::cout << "Hello World!\n";
